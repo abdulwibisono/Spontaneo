2213e8f3f1db1e17f8f462863f019c94b7308173
abdulwibisono <mughniwibisono@gmail.com>
2024-08-26 22:20:30 +1000

    Initial Commit

---
+++ Project/Spontaneo/SpontaneoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpontaneoApp.swift
+//  Spontaneo
+//
+//  Created by Mughni Wibisono on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SpontaneoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
