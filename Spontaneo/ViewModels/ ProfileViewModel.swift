bf477a20552e97c1c4fd3658cfeadc0f1468da25
syafiqo.ararashy <syafiqo.arashy@ui.ac.id>
2024-10-21 12:36:52 +1000

    chore: Clean code part 2

--- Spontaneo/ViewModels/ ProfileViewModel.swift
+++ Spontaneo/ViewModels/ ProfileViewModel.swift
@@ -21,6 +21,47 @@ class ProfileViewModel: ObservableObject {
         fetchUser(userId: userId)
     }
     
+    private func fetchUser(userId: String) {
+        isLoading = true
+        userService.fetchUser(userId: userId)
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] completion in
+                self?.isLoading = false
+                if case .failure(let error) = completion {
+                    self?.errorMessage = error.localizedDescription
+                }
+            } receiveValue: { [weak self] user in
+                self?.user = user
+            }
+            .store(in: &cancellables)
+    }
+    
+    var formattedJoinDate: String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        return formatter.string(from: user.joinDate)
+    }
+    
+    func updateProfile(fullName: String, bio: String) {
+        isLoading = true
+        var updatedUser = user
+        updatedUser.fullName = fullName
+        updatedUser.bio = bio
+        
+        userService.updateUser(updatedUser)
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] completion in
+                self?.isLoading = false
+                if case .failure(let error) = completion {
+                    self?.errorMessage = error.localizedDescription
+                }
+            } receiveValue: { [weak self] updatedUser in
+                self?.user = updatedUser
+            }
+            .store(in: &cancellables)
+    }
+    
     func uploadProfileImage(_ image: UIImage) {
         isLoading = true
         guard let imageData = image.jpegData(compressionQuality: 0.5) else {
@@ -76,5 +117,34 @@ class ProfileViewModel: ObservableObject {
             }
             .store(in: &cancellables)
     }
-
+    
+    func addInterest(_ interest: String) {
+        var updatedInterests = user.interests
+        updatedInterests.append(interest)
+        updateInterests(updatedInterests)
+    }
+    
+    func removeInterests(at offsets: IndexSet) {
+        var updatedInterests = user.interests
+        updatedInterests.remove(atOffsets: offsets)
+        updateInterests(updatedInterests)
+    }
+    
+    private func updateInterests(_ interests: [String]) {
+        isLoading = true
+        var updatedUser = user
+        updatedUser.interests = interests
+        
+        userService.updateUser(updatedUser)
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] completion in
+                self?.isLoading = false
+                if case .failure(let error) = completion {
+                    self?.errorMessage = error.localizedDescription
+                }
+            } receiveValue: { [weak self] updatedUser in
+                self?.user = updatedUser
+            }
+            .store(in: &cancellables)
+    }
 }
